9dfdfe7e9c6e1c9a38b61cb5816763b4a545fd98
Rafi s1312017 <putrarafi252@gmail.com>
2025-12-19 16:07:52 +0900

    Add imports for sorting algorithms
    Import sorting algorithms from various modules.

--- Sorting Algorithm/main.py
+++ Sorting Algorithm/main.py
@@ -1,1 +1,7 @@
-testing
+from Heap_Sort import heapSort
+from Bubble_Sort import bubbleSort
+from Selection_Sort import selectionSort
+from insertion_sort import insertionSort
+from merge_sort import mergeSort
+from quick_sort import quickSort
+
