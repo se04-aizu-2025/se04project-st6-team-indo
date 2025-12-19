106728720166185baa83c7e17cd0092a73910d1d
RichardReaganHB <richard.rrhb@gmail.com>
2025-12-19 14:39:44 +0700

    Add files via upload

--- Sorting Algorithm/main.py
+++ Sorting Algorithm/main.py
@@ -1,3 +1,10 @@
+from Heap_Sort import heapSort
+from Bubble_Sort import bubbleSort
+from Selection_Sort import selectionSort
+from insertion_sort import insertionSort
+from merge_sort import mergeSort
+from quick_sort import quickSort
+
 import numpy as np
 
 random_array = np.random.randint(0, 100, size=50)
